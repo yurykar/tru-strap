4156129137325823d5987a09a92d13f1cc0ad3e3
cornelf <cornelf@cakesolutions.net>
2014-08-08 15:53:42 +0100

    use ruby 2.1.1 instead of latest

--- init.sh
+++ init.sh
@@ -133,7 +133,8 @@ puppet apply -e "file { '/etc/hiera.yaml': ensure => link, target => '/etc/puppe
 echo "Installing RVM and latest Ruby"
 curl -sSL https://get.rvm.io | bash
 source /usr/local/rvm/scripts/rvm
-rvm install $(rvm list remote | grep ruby | tail -1 | awk '{ print $NF }') --binary
+rvm install 2.1.1
+#rvm install $(rvm list remote | grep ruby | tail -1 | awk '{ print $NF }') --binary
 
 # # Use RVM to select specific Ruby version (2.1+) for use with Librarian-puppet
 rvm use ruby
