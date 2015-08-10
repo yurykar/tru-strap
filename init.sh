f0018e26634f88a67bfc68ee8e91c7194ecd10df
Marcin Jedyk <Marcin.Jedyk@moneysupermarket.com>
2015-08-10 15:45:58 +0100

    use double [

--- init.sh
+++ init.sh
@@ -310,7 +310,7 @@ fetch_puppet_modules() {
 
   if [[ ! -z "${FACTER_init_moduleshttpcache}" && "200" == $(curl "${FACTER_init_moduleshttpcache}"/"${MODULE_ARCH}"  --head --silent | head -n 1 | cut -d ' ' -f 2) ]]; then
     echo -n "Downloading pre-packed Puppet modules from cache..."
-    if [ ! -d $PASSWD ]; then
+    if [[ ! -d $PASSWD ]; then
       echo "================="
       echo "Using Encrypted modules"
       echo "================="
@@ -321,7 +321,7 @@ fetch_puppet_modules() {
       curl -o modules.tar.gz ${FACTER_init_moduleshttpcache}/$MODULE_ARCH
     fi
     
-    if [ $DECRYPT_EXIT_CODE -eq 0 ]; then
+    if [[ $DECRYPT_EXIT_CODE -eq 0 ]]; then
       tar zxpf modules.tar.gz
       echo "================="
       echo "Unpacked modules:"
